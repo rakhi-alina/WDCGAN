c9c5c88466270634e3c6975c62adc9cc7b89a4c2
fracogno <francesco.cognolato@hotmail.com>
2019-06-08 00:36:51 +1000

    Good

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import os
 from keras.datasets import mnist
+import cv2
+import matplotlib.image as mpimg
+import os
 
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]="0"  # specify which GPU(s) to be used
@@ -16,10 +18,15 @@ def normalize(vector, a, b):
 
     return result
 
-(X_train, Y_train), _ = mnist.load_data()
-
+(X_tmp, Y_train), _ = mnist.load_data()
+X_train = []
+for img in X_tmp:
+    #tmp = cv2.resize(img, (int(64),int(64)), interpolation = cv2.INTER_CUBIC)
+    #X_train.append(tmp)
+    X_train.append(img)
+    
 # Expand to have 1 channel (grey images)
-X_train = np.expand_dims(X_train, axis=3)
+X_train = np.expand_dims(X_train, axis=-1)
 print(X_train.shape)
 
 # Print max and min and normalize
@@ -28,96 +35,124 @@ X_train = normalize(X_train, -1, 1)
 print("Normalized MAX : " + str(X_train.max()) + " and MIN: " + str(X_train.min()))
 
 assert(X_train.shape[1] == X_train.shape[2])
+base_path = "/home/francesco/UQ/Job/Tumour_GAN/"
 
 # Parameters network and training
-epochs = 500
-batchSize = 32
-lr = 1e-4
+epochs = 1000
+batchSize = 128
+lr = 0.0002
 Z_dim = 100
 mu, sigma = 0, 1
 
-# Sample noise for generator
-def sample_Z(batch_size, img_size, mu, sigma):
-    return np.random.normal(mu, sigma, size=[batch_size, img_size])
-    #return np.random.uniform(-1., 1., size=[batch_size, img_size])
+
+def sample_noise(batch_size, size, mu, sigma):
+    #return np.random.normal(mu, sigma, size=[batch_size, 1, 1, size])
+    return np.random.uniform(-1., 1., size=[batch_size, size])
 
 
-def generator(z, reuse=False):
+def generator(z, trainable, reuse=False):
     
-    with tf.variable_scope("generator", reuse=reuse):        
-        fc1 = tf.layers.dense(inputs=z, units=256, activation=tf.nn.leaky_relu)
-        fc2 = tf.layers.dense(inputs=fc1, units=784, activation=None)
-        logits = tf.nn.tanh(fc2)
-        output = tf.reshape(logits, shape=[-1, 28, 28, 1])
+    with tf.variable_scope('generator', reuse=reuse):
         
-        print("Generator:")
+        fc1 = tf.layers.dense(z, 2*2*512)
+        
+        deconv_2 = tf.reshape(fc1, (-1, 2, 2, 512))
+        bn2 = tf.layers.batch_normalization(deconv_2, training=trainable)
+        lrelu2 = tf.nn.leaky_relu(bn2)
+        
+        deconv3 = tf.layers.conv2d_transpose(lrelu2, 256, 5, 2, padding='VALID')
+        bn3 = tf.layers.batch_normalization(deconv3, training=trainable)
+        lrelu3 = tf.nn.leaky_relu(bn3)
+        
+        deconv4 = tf.layers.conv2d_transpose(lrelu3, 128, 5, 2, padding='SAME')
+        bn4 = tf.layers.batch_normalization(deconv4, training=trainable)
+        lrelu4 = tf.nn.leaky_relu(bn4)
+        
+        deconv5 = tf.layers.conv2d_transpose(lrelu4, 1, 5, 2, padding='SAME')
+        output = tf.tanh(deconv5)
+        
         print(z)
         print(fc1)
-        print(fc2)
-        print(logits)
+        print(lrelu2)
+        print(lrelu3)
+        print(lrelu4)
         print(output)
         
     return output
-
-
-def discriminator(x, reuse=False):
     
-    with tf.variable_scope("discriminator", reuse=reuse):
-        x = tf.layers.Flatten()(x)
-        fc1 = tf.layers.dense(inputs=x, units=256, activation=tf.nn.leaky_relu)
-        fc2 = tf.layers.dense(inputs=fc1, units=1, activation=None)
-        prob = tf.nn.sigmoid(fc2)
-        
-        if not reuse:
-            print("\nDiscriminator:")
-            print(x)
-            print(fc1)
-            print(fc2)
-            print(prob)
-        
-    return prob
+
+def discriminator(x, trainable, reuse=False):
+    
+    with tf.variable_scope('discriminator', reuse=reuse):
+        conv1 = tf.layers.conv2d(x, 64, 5, 2, 'SAME')
+        lrelu1 = tf.nn.leaky_relu(conv1)
+
+        conv2 = tf.layers.conv2d(lrelu1, 128, 5, 2, 'SAME')
+        bn2 = tf.layers.batch_normalization(conv2, training=trainable)
+        lrelu2 = tf.nn.leaky_relu(bn2)
+
+        conv3 = tf.layers.conv2d(lrelu2, 256, 5, 1, 'SAME')
+        bn3 = tf.layers.batch_normalization(conv3, training=trainable)
+        lrelu3 = tf.nn.leaky_relu(bn3)
+
+        reshaped = tf.reshape(lrelu3, (-1, 4*4*256))
+        logits = tf.layers.dense(reshaped, 1)
+        probability = tf.sigmoid(logits)
+    
+    if not reuse:
+        print(x)
+        print(lrelu1)
+        print(lrelu2)
+        print(lrelu3)
+        print(reshaped)
+        print(probability)
+
+    return probability, logits
 
 
 tf.reset_default_graph()
 
-# Generator noise input
+# Inputs
 Z = tf.placeholder(tf.float32, shape=[None, Z_dim], name='Z')
-
-# Discriminator input
 X = tf.placeholder(tf.float32, shape=[None, X_train.shape[1], X_train.shape[2], X_train.shape[3]], name='X')
+isTrain = tf.placeholder(dtype=tf.bool)
 
 # Networks
-G_z = generator(Z)
+G_z = generator(Z, isTrain)
+_, D_logits_real = discriminator(X, isTrain)
+_, D_logits_fake = discriminator(G_z, isTrain, reuse=True)
 
-D_real = discriminator(X)
-D_fake = discriminator(G_z, reuse=True)
 
+def discriminatorLoss(D_logits_real, D_logits_fake, label_smoothing=1):
+    D_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logits_real,
+                                                labels=tf.ones_like(D_logits_real) * label_smoothing))
+    D_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logits_fake,
+                                                labels=tf.zeros_like(D_logits_fake)))
+    D_loss = D_loss_real + D_loss_fake
+    
+    return D_loss
+
+def generatorLoss(D_logits_fake):
+    G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logits_fake,
+                                                labels=tf.ones_like(D_logits_fake)))
+    return G_loss
+
+D_loss = discriminatorLoss(D_logits_real, D_logits_fake, 0.9)
+G_loss = generatorLoss(D_logits_fake)
 
 # Losses have minus sign because I have to maximize them
-D_loss = - tf.reduce_mean( tf.log(D_real) + tf.log(1. - D_fake) )
-G_loss = - tf.reduce_mean( tf.log(D_fake) )
+#D_loss = - tf.reduce_mean(tf.log(D_real) + tf.log(1 - D_fake))
+#G_loss = - tf.reduce_mean(tf.log(D_fake))
 
+all_vars = tf.trainable_variables()
+D_vars = [var for var in all_vars if var.name.startswith('discriminator')]
+G_vars = [var for var in all_vars if var.name.startswith('generator')]
 
-# Optimizers
-D_var = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='discriminator')
-D_optimizer = tf.train.AdamOptimizer(learning_rate=lr).minimize(D_loss, var_list=D_var)
-
-G_var = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='generator')
-G_optimizer = tf.train.AdamOptimizer(learning_rate=lr).minimize(G_loss, var_list=G_var)
-
-
-def saveImages(images, globalStep, samples=9):    
-    size = np.sqrt(samples)
-    fig = plt.figure(figsize=(15, 15))
-
-    for i in range(np.minimum(samples, len(images))):
-        plt.subplot(size, size, i+1)
-        plt.imshow(np.squeeze(images[i]), cmap='gray')
-        plt.axis('off')
-
-    plt.savefig('./images/image_' + str(globalStep) + '.png')
-    #plt.show()
-    plt.close(fig)
+# Optimize
+update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+with tf.control_dependencies(update_ops): 
+    D_optimizer = tf.train.AdamOptimizer(lr, beta1=0.5).minimize(D_loss, var_list=D_vars)
+    G_optimizer = tf.train.AdamOptimizer(lr, beta1=0.5).minimize(G_loss, var_list=G_vars)
 
 
 tf.summary.scalar('D_loss', D_loss)
@@ -127,43 +162,48 @@ tf.summary.scalar('G_loss', G_loss)
 merged_summ = tf.summary.merge_all()
 
 # VISUALIZE => tensorboard --logdir=.
-summaries_dir = "./checkpoints"
+summaries_dir = base_path + "checkpoints"
+
+# Test noise
+testNoise = sample_noise(6, Z_dim, mu, sigma)
+print(testNoise.shape)
+
+def saveImages(images, epoch):    
+    for i in range(len(images)):
+        mpimg.imsave(base_path + "images/out-" + str(epoch) + "-" + str(i) + ".png", np.squeeze(G_output[i]))
 
 
 indices = list(range(len(X_train)))
 
 with tf.Session() as sess:
 
-    # Run the initializer
     saver = tf.train.Saver()
     sess.run(tf.global_variables_initializer())
-    
     summary_writer = tf.summary.FileWriter(summaries_dir, graph=tf.get_default_graph())
     
-    # Epochs
-    globalStep = 1
-    for i in range(epochs):
-        
+    globalStep = 0
+    for i in range(epochs):        
         # Shuffle dataset every epoch
         print("Epoch " + str(i))
         np.random.shuffle(indices)
         X_train = X_train[indices]
         
         for j in range(0, len(X_train), batchSize):
-            
-            # Sample noise
-            noise = sample_Z(len(X_train[j:j+batchSize]), Z_dim, mu, sigma)
-            
-            _ = sess.run(D_optimizer, feed_dict={ X: X_train[j:j+batchSize], 
-                                                  Z: noise })
+            noise = sample_noise(len(X_train[j:j+batchSize]), Z_dim, mu, sigma)
 
+            _ = sess.run(D_optimizer, feed_dict={ X: X_train[j:j+batchSize], 
+                                                  Z: noise,
+                                                  isTrain: True})
+
+            noise = sample_noise(len(X_train[j:j+batchSize]), Z_dim, mu, sigma)
             _, summary = sess.run([G_optimizer, merged_summ], feed_dict={ X: X_train[j:j+batchSize],
-                                                                          Z: noise })
+                                                                          Z: noise,
+                                                                          isTrain: True })
+            globalStep+=1
             summary_writer.add_summary(summary, globalStep)
-            
-            globalStep += 1
-        
+
         # Check results every epoch
-        save_path = saver.save(sess, "./checkpoints/model.ckpt")                
-        output = sess.run(G_z, feed_dict={ Z: noise })
-        saveImages(output, globalStep)
+        save_path = saver.save(sess, base_path + "checkpoints/model.ckpt")
+        G_output = sess.run(G_z, feed_dict={ Z: testNoise,
+                                            isTrain: False })
+        saveImages(G_output, i)
